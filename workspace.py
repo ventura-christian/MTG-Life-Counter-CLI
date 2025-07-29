36dd5860cdf67e7b99d6cb20b05bff430f6aa3e4
[Christian Ventura] <[cventura64@outlook.com]>
2025-07-29 09:52:48 -0700

    making refinments and trying to figure out how to properly store data in variables and update the information in real time

--- workspace.py
+++ workspace.py
@@ -2,23 +2,16 @@
 # This will be my first project that I will code from scratch using concepts I'm learning on Boot.dev
 # This is a work in progress so take my commits as steps towards the final completion of this idea
 
-# def player_one():
+# Pseudocode
 
-
-# def player_two():
-
-
-# def main():
-#     print("-----------------")
-#     print(f"Player One life total: {life_total}")
-#     print(f"Player Two life total: {life_total}")
 
 def player(life_total, damage, healing):
+    life_total = 
     life_total += healing
     life_total -= damage
     remaining_life = life_total
     return remaining_life
 
-# print(player(20, 6, 0))
+print(player(20, 0, 10))
     
     
