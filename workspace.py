6061173b187a384fe1462520325914e55f09f8e8
[Christian Ventura] <[cventura64@outlook.com]>
2025-08-09 11:57:21 -0700

    Adjusted the layout of the code for readability.

--- workspace.py
+++ workspace.py
@@ -1,17 +1,3 @@
-# MTG LIFE COUNTER CLI APPLICATION
-# This will be my first project that I will code from scratch using concepts I'm learning on Boot.dev
-# This is a work in progress so take my commits as steps towards the final completion of this idea
-
-# What do I want to achieve?
-# 1: I want to define the number of players
-# 2: I want to create life totals for the designated amount of players
-# 3: I want to print the players and their associated life totals
-# 4: I want to assign damage or healing to any of the players then return the current life totals after interaction
-# 5: I want to store the current state of the life totals for the respective players and update the life totals after each interaction
-# 6: I want to print a clean statement that displays all of the players names and current life totals after each interaction and will prompt for more interactions 
-
-# Current attempt to create a player and the code to handle the interaction math
-
 print("========================")
 print("Welcome to AetherPulse\nThe simple to use Magic the Gathering life tracker")
 print("========================")
@@ -31,6 +17,8 @@ def player_options():
             current_life_total = calcultion
             print(f"{player_name} now has {current_life_total} life.") 
             return player_options()
+        # THIS IS WHERE I LEFT OFF!!
+        # Continue figuring out how to store the new, updated life totals after each change occurs
         elif options == 2:
             life_lost = input("How much life is lost? ")
             current_life_total = starting_life_total - int(life_lost)
@@ -43,23 +31,21 @@ print(player_options())
             
     
     
-# print(player_options())
+
+    
+# MTG LIFE COUNTER CLI APPLICATION
+# This will be my first project that I will code from scratch using concepts I'm learning on Boot.dev
+# This is a work in progress so take my commits as steps towards the final completion of this idea
+
+# What do I want to achieve?
+# 1: I want to define the number of players
+# 2: I want to create life totals for the designated amount of players
+# 3: I want to print the players and their associated life totals
+# 4: I want to assign damage or healing to any of the players then return the current life totals after interaction
+# 5: I want to store the current state of the life totals for the respective players and update the life totals after each interaction
+# 6: I want to print a clean statement that displays all of the players names and current life totals after each interaction and will prompt for more interactions 
+    
         
-
-
-# I need to take the player name and current life total and interact with them using logic to add or remove health from the player
-# def life_gain():
-    
-    
-        
-    
-
-
-    
-    
-
-    
-
 # def player_status:
 # I need this to accept inputs for taking damage and gaining health. 
 # I need to assign this to each player individually and allow for each player to be affected without changing the other players life totals or status changes
